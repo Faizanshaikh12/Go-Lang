e78735c8dca13c9bec3a6298a6a05cb480934f78
Faizan Shaikh <faizanroy000777@gmail.com>
2024-09-05 19:35:24 +0530

    4. constants

---
+++ 4_constants/constants.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const name = "Faizan"
+	// fmt.Println(name)
+
+	// group constant
+	const (
+		fname = "Faizan"
+		lname = "Shaikh"
+	)
+
+	fmt.Println(lname, fname)
+}
